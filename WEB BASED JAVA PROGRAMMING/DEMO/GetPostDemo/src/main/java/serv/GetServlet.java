769149691585e29d911431b7fdda85ced96b63f2
prajwal <prajwalthete98@gmail.com>
2022-11-24 20:12:31 +0530

    commit

---
+++ WEB BASED JAVA PROGRAMMING/DEMO/GetPostDemo/src/main/java/serv/GetServlet.java
@@ -0,0 +1,29 @@
+package serv;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(value = { "/GetServlet" })
+public class GetServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// specify the response content type
+		resp.setContentType("text/plain");
+		// get PrintWriter object of the Response to write the response text
+		PrintWriter out = resp.getWriter();
+		// write the response
+		out.print("Hello from doGet");
+		// flush the content / empty the buffer
+		out.flush();
+		// close the resource PrintWriter
+		out.close();
+	}
+
+}
